bac7fe10c57c10382e7141c81e5a0f5f96c1877b
Eduard Tokarev <etdevios@gmail.com>
2022-08-29 21:32:45 +0300

    feat!: Подготовил Tv и Movies модели

---
+++ TGMovve/Models/Movies.swift
@@ -0,0 +1,15 @@
+//
+//  Movies.swift
+//  TGMovve
+//
+//  Created by Eduard Tokarev on 29.08.2022.
+//
+
+import Foundation
+
+struct Movies {
+    let id: Int
+    let title: String
+    let poster_path: String?
+    let release_date: String
+}
---
+++ TGMovve/Models/Tv.swift
@@ -0,0 +1,16 @@
+//
+//  Tv.swift
+//  TGMovve
+//
+//  Created by Eduard Tokarev on 29.08.2022.
+//
+
+import Foundation
+
+struct Tv {
+    let id: Int
+    let name: String
+    let poster_path: String?
+    let first_air_date: String
+}
+
