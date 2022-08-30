77194f2a48c41214db37ddd45fa78422b712a97c
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-30 15:29:10 +0300

    modify models add protocol ShowRepresentable apply this protocol to MovieInfo and TVSeriesInfo models

--- TGMovve/Models/TVSeriesInfo.swift
+++ TGMovve/Models/TVSeriesInfo.swift
@@ -5,17 +5,22 @@
 //  Created by Eduard Tokarev on 29.08.2022.
 //
 
-import Foundation
-
-struct TVSeriesInfo: Decodable {
+struct TVSeriesInfo: Decodable, ShowRepresentable {
+    var title: String {
+        return name
+    }
+    
+    var releaseDate: String {
+        return firstAirDate
+    }
+    
     let posterPath: String?
     let id: Int
     let name: String
-    let tagline: String
+    let tagline: String?
     let firstAirDate: String
     let voteAverage: Float
     let genres: [Genre]
-    let overview: String
-    let homepage: String
-    
+    let overview: String?
+    let homepage: String?
 }
