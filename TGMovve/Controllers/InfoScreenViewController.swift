36ec271881b76a9791128d7978203e5438da8f23
Alexander Altman <qewhouse@gmail.com>
2022-08-31 01:41:17 +0300

    Делаю Ui экрана info

---
+++ TGMovve/Controllers/InfoScreenViewController.swift
@@ -0,0 +1,28 @@
+//
+//  InfoScreenViewController.swift
+//  TGMovve
+//
+//  Created by Alexander Altman on 30.08.2022.
+//
+
+import UIKit
+
+class InfoScreenViewController: UIViewController {
+    
+    @IBOutlet weak var PosterImage: UIImageView!
+    @IBOutlet weak var VideoNameLabel: UILabel!
+    @IBOutlet weak var InfoLabel: UILabel!
+    @IBOutlet weak var RaitingLabel: UILabel!
+    @IBOutlet weak var DescriptionLabel: UILabel!
+    
+    @IBOutlet weak var DetailsButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+}
---
+++ TGMovve/Views/CastsCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CastsCollectionViewCell.swift
+//  TGMovve
+//
+//  Created by Alexander Altman on 31.08.2022.
+//
+
+import UIKit
+
+class CastsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var castImageView: UIImageView!
+    @IBOutlet weak var backgroundColorView: UIView!
+    @IBOutlet weak var actorName: UILabel!
+    @IBOutlet weak var castPerson: UILabel!
+}
