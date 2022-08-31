15c93a27ed0b3f5235f39e3588ff50d7d871c6cb
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-31 03:01:11 +0300

    try to make composition layout

---
+++ TGMovve/Controllers/ContentCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ContentCollectionViewCell.swift
+//  TGMovve
+//
+//  Created by ALEKSEY SUSLOV on 31.08.2022.
+//
+
+import UIKit
+
+class ContentCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var posterImage: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+}
--- TGMovve/Controllers/HomeCollectionViewController.swift
+++ TGMovve/Controllers/HomeCollectionViewController.swift
@@ -9,40 +9,110 @@ import UIKit
 
 class HomeCollectionViewController: UICollectionViewController {
     
-    let content: [String: [ContentRepresentable]?] = [
+    let compositionalLayout: UICollectionViewCompositionalLayout = {
+        let inset: CGFloat = 2.5
+        
+        // Items
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: inset, leading: inset, bottom: inset, trailing: inset)
+        
+        // Outer Group
+        let outerGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.5))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: outerGroupSize, subitems: [item])
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: inset, leading: inset, bottom: inset, trailing: inset)
+        section.orthogonalScrollingBehavior = .groupPaging
+        
+        // Supplementary Item
+//        let headerItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(100))
+//        let headerItem = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerItemSize, elementKind: "header", alignment: .top)
+//        section.boundarySupplementaryItems = [headerItem]
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }()
+    
+    
+    
+    let contentList: [String: [ContentRepresentable]] = [
         "Popular Movies": Movie.getMovies(),
         "TV Shows": TVSeries.getTVSeries()
     ]
+    
+    let flowLayout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 5
+        layout.minimumLineSpacing = 5
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        return layout
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerCells()
+        collectionView.collectionViewLayout = compositionalLayout
+        collectionView.reloadData()
     }
     
+    private func registerCells() {
+//        collectionView.register(CollectionReusableView.self, forSupplementaryViewOfKind: "header", withReuseIdentifier: "ContentHeader")
+        
+//        collectionView.register(UINib(nibName: "PhotoCell", bundle: nil), forCellWithReuseIdentifier: "PhotoCell")
+//        collectionView.register(UINib(nibName: "HeaderSupplementaryView", bundle: nil), forSupplementaryViewOfKind: "header", withReuseIdentifier: "HeaderSupplementaryView")
+//        collectionView.register(UINib(nibName: "NewBannerSupplementaryView", bundle: nil), forSupplementaryViewOfKind: "new-banner", withReuseIdentifier: "NewBannerSupplementaryView")
+    }
 
     // MARK: UICollectionViewDataSource
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return content.values.count
+        return contentList.values.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let view = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "ContentTitleView", for: indexPath) as! CollectionReusableView
-        view.groupNameLabel.text = Array(content.keys)[indexPath.section]
+        let view = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "ContentHeader", for: indexPath) as! CollectionReusableView
+        view.groupNameLabel.text = Array(contentList.keys)[indexPath.section]
         return view
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         var numberOfItems = 0
         
-        let valueKey = Array(content.keys)[section]
-        let contentType = content[valueKey]
-        numberOfItems = contentType??.count ?? 0
+        let valueKey = Array(contentList.keys)[section]
+        let contentType = contentList[valueKey]
+        numberOfItems = contentType?.count ?? 0
         
         return numberOfItems
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentCell", for: indexPath)
-        // Configure the cell
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentCell", for: indexPath) as? ContentCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let valueKey = Array(contentList.keys)[indexPath.section]
+        
+        if let contentArray = contentList[valueKey] {
+            let content = contentArray[indexPath.item]
+            
+            cell.titleLabel.text = content.title
+            cell.dateLabel.text = content.releaseDate
+            
+            
+            DispatchQueue.global().async {
+                URLManager.get.mediumImageFor(content.posterPath!) { imageURL in
+                    guard let imageData = try? Data(contentsOf: imageURL) else { return }
+                    DispatchQueue.main.async {
+                        cell.posterImage.image = UIImage(data: imageData)
+                    }
+                }
+            }
+            
+        }
+        
+        
+        
+        
         return cell
     }
 
@@ -82,6 +152,13 @@ class HomeCollectionViewController: UICollectionViewController {
 extension HomeCollectionViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 //        return CGSize(width: UIScreen.main.bounds.width, height: 200)
-        return CGSize(width: 150, height: 200)
+//        return CGSize(width: 150, height: 200)
+        
+        let width = collectionView.bounds.width
+        let numberOfItemsPerRow: CGFloat = 2
+        let spacing: CGFloat = flowLayout.minimumInteritemSpacing
+        let availableWidth = width - spacing * (numberOfItemsPerRow + 1)
+        let itemDimension = floor(availableWidth / numberOfItemsPerRow)
+        return CGSize(width: itemDimension, height: itemDimension)
     }
 }
