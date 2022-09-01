a5fefd0996a8fa9b76210fa7925198e314585439
AntonErmokhin <anton1ntense@gmail.com>
2022-09-02 00:35:59 +0400

    delete viewDidLoad

--- TGMovve/Controllers/FavoritesViewController.swift
+++ TGMovve/Controllers/FavoritesViewController.swift
@@ -12,19 +12,6 @@ class FavoritesViewController: UITableViewController {
     
     var contentList = [Content]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        loadContent()
-        
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
