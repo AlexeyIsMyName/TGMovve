4971d8168d29673edda20fc4ab2637d6de6bb947
Alexander Altman <qewhouse@gmail.com>
2022-09-02 23:57:18 +0300

    info screen added features: * bookmark * back button * details button * movie images * cast images

--- TGMovve/Controllers/HomeCollectionViewController.swift
+++ TGMovve/Controllers/HomeCollectionViewController.swift
@@ -112,33 +112,6 @@ class HomeCollectionViewController: UICollectionViewController {
     // MARK: UICollectionViewDelegate
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         performSegue(withIdentifier: "ShowDetailsFromHome", sender: nil)
-        
-        let valueKey = Array(contentList.keys)[indexPath.section]
-        
-        if let contentArray = contentList[valueKey] {
-            let content = contentArray[indexPath.item]
-            let newContent = Content(context: self.context)
-            newContent.id = Int32(content.id)
-            newContent.title = content.title
-            newContent.posterPath = content.posterPath
-            newContent.releaseData = content.releaseDate
-            
-            if let _ = content as? Movie {
-                newContent.type = "Movie"
-            }
-            
-            if let _ = content as? TVSeries {
-                newContent.type = "TVSeries"
-            }
-            
-            
-            
-            do {
-                try context.save()
-            } catch {
-                print("Error saving context \(error)")
-            }
-        }
     }
     
     
--- TGMovve/Controllers/InfoScreenViewController.swift
+++ TGMovve/Controllers/InfoScreenViewController.swift
@@ -6,20 +6,21 @@
 //
 
 import UIKit
+import CoreData
 
 class InfoScreenViewController: UIViewController {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     @IBOutlet weak var posterImage: UIImageView!
     @IBOutlet weak var videoNameLabel: UILabel!
     @IBOutlet weak var infoLabel: UILabel!
     @IBOutlet weak var raitingLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
-    
     @IBOutlet weak var collectionView: UICollectionView!
-    
-    
     @IBOutlet var stars: [UIImageView]!
-    
+    @IBOutlet weak var detailsButton: UIButton!
+    @IBOutlet weak var bookmarkButton: UIBarButtonItem!
     
     
     var show: ShowRepresentable!
@@ -27,7 +28,11 @@ class InfoScreenViewController: UIViewController {
     
     var runtime: String {
         guard let runtime = show.runtime else {return ""}
-        return "\(runtime / 60) h \(runtime - (runtime / 60) * 60) m"
+        return ", \(runtime / 60) h \(runtime - (runtime / 60) * 60) m"
+    }
+    
+    var genre: String {
+        show.genres.map {$0.name}.joined(separator: ", ")
     }
     
     override func viewDidLoad() {
@@ -36,13 +41,59 @@ class InfoScreenViewController: UIViewController {
     }
     
     
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func bookmarkButtonPressed(_ sender: Any) {
+        
+        let newContent = Content(context: self.context)
+        newContent.id = Int32(show.id)
+        newContent.title = show.title
+        newContent.posterPath = show.posterPath
+        newContent.releaseData = show.releaseDate
+        
+        if let _ = show as? MovieInfo {
+            newContent.type = "Movie"
+        }
+        
+        if let _ = show as? TVSeriesInfo {
+            newContent.type = "TVSeries"
+        }
+        
+        do {
+            try context.save()
+            bookmarkButton.image = UIImage(systemName: "bookmark.fill")
+        } catch {
+            print("Error saving context \(error)")
+        }
+    }
+    
+    @IBAction func detailsButtonPressed(_ sender: Any) {
+        UIApplication.shared.open(URL(string: show.homepage!)!)
+    }
+    
     
     func updateUI() {
         videoNameLabel.text = show.title
-        infoLabel.text = "\(show.releaseDate.prefix(4)), Жанр, \(runtime)"
-        
+        infoLabel.text = "\(show.releaseDate.prefix(4)), \(genre) \(runtime)"
+        if show.homepage != nil {
+            detailsButton.isEnabled = true
+        }
         setRating()
         descriptionLabel.text = show.overview
+        
+        if let posterURL = show.posterPath {
+            DispatchQueue.global().async {
+                URLManager.get.largeImageFor(posterURL) { imageURL in
+                    guard let imageData = try? Data(contentsOf: imageURL) else { return }
+                    DispatchQueue.main.async {
+                        self.posterImage.image = UIImage(data: imageData)
+                    }
+                }
+            }
+        }
+        
     }
     
     func prepareWith(_ content: ContentRepresentable) {
@@ -68,17 +119,16 @@ class InfoScreenViewController: UIViewController {
             getTVSeriesInfoFor(id: Int(content.id))
         }
     }
-    
-    
 }
 
 //MARK: UICollectionViewDataSource
 extension InfoScreenViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        cast?.count ?? 0
+        print(cast?.count ?? 0)
+        return cast?.count ?? 0
     }
-
+    
     
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -87,13 +137,20 @@ extension InfoScreenViewController: UICollectionViewDataSource {
         }
         
         if let cast = cast?[indexPath.item] {
-                castCell.actorName.text = cast.name
-                castCell.castName.text = cast.character
+            castCell.actorName.text = cast.name
+            castCell.castName.text = cast.character
+            
+            if let posterURL = cast.profilePath {
+                DispatchQueue.global().async {
+                    URLManager.get.smallImageFor(posterURL) { imageURL in
+                        guard let imageData = try? Data(contentsOf: imageURL) else { return }
+                        DispatchQueue.main.async {
+                            castCell.actorImage.image = UIImage(data: imageData)
+                        }
+                    }
+                }
+            }
         }
-        
-        // дописать реализацию получения картинки
-        
-        
         return castCell
     }
 }
